3c6317f488147da3864956150bc6838c2ab22a5d
Luke Lai <lukelai.dev@gmail.com>
2023-01-16 15:29:54 +0000

    Add test to string_builder_spec

--- spec/string_builder_spec.rb
+++ spec/string_builder_spec.rb
@@ -5,5 +5,19 @@ describe StringBuilder do
     builder = StringBuilder.new
     result = builder.add("Tony")
     expect(result).to eq("Tony")    
-  end    
+  end
+  
+  it 'should return 4 as the length of "Tony"' do
+    builder = StringBuilder.new
+    builder.add('Tony')
+    result = builder.size
+    expect(result).to eq 4
+  end
+
+  it 'should return "Tony"' do
+    builder = StringBuilder.new
+    builder.add('Tony')
+    result = builder.output
+    expect(result).to eq 'Tony'
+  end
 end
