fb449daffaa65914e76c08d92204e64a17775669
Max Allan-Smith <Maxibon_1195@hotmail.com>
2023-01-18 10:19:19 +0000

    Update

--- personal-diary/lib/personal_diary.rb
+++ personal-diary/lib/personal_diary.rb
@@ -3,7 +3,7 @@
 class PersonalDiary
 
   def make_snippet(str)
-    # Splits the string words into an array of elements
+    
     splitResult = str.split(" ")
 
     count = splitResult.length
@@ -13,6 +13,7 @@ class PersonalDiary
     elsif count > 5
       return "One Two Three Four Five ..."
     end
+
   end
 
   def count_words(str)
