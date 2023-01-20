fe65ec711a26f30180bdbcce3ae47164228ce7f4
Max Allan-Smith <Maxibon_1195@hotmail.com>
2023-01-20 19:14:37 +0000

    Wrote tests for 'check' method and implemented the code to pass the tests.

--- Test-Drive-a-Single-Class-Program/challenge/lib/challenge.rb
+++ Test-Drive-a-Single-Class-Program/challenge/lib/challenge.rb
@@ -1,12 +1,26 @@
 class GrammarStats
   def initialize
-    # ...
+    #...
   end
 
   def check(text) 
     # text is a string
     # Returns true or false depending on whether the text begins with a capital
     # letter and ends with a sentence-ending punctuation mark.
+
+    case 
+    when text[0].scan(/[A-Z]/).any? == false && text.scan(/[.!?]/).any? == false
+      return false
+    when text[0].scan(/[A-Z]/).any? == true && text.scan(/[.!?]/).any? == false
+      return false
+    when text[0].scan(/[A-Z]/).any? == false && text.scan(/[.!?]/).any? == true
+      return false
+    when text[0].scan(/[A-Z]/).any? && text.scan(/[.!?]/).any?
+      return true
+    else
+      
+    end
+    
   end
 
   def percentage_good
@@ -14,3 +28,7 @@ class GrammarStats
     # the check defined in the `check` method. The number 55 represents 55%.
   end
 end
+
+run = GrammarStats.new
+
+puts run.check("Hello")
--- Test-Drive-a-Single-Class-Program/challenge/spec/challenge_spec.rb
+++ Test-Drive-a-Single-Class-Program/challenge/spec/challenge_spec.rb
@@ -1,10 +1,29 @@
 require 'challenge'
 
 describe GrammarStats do
-  context "Check Error:" do
-    it "String Does Not Start with a Capital Letter." do
+  context "'Check' Method Tests:" do
+    it "Should return as FALSE when string does not start without a capital letter or a punctuation mark." do
       grammerStats = GrammarStats.new
-      expect(grammerStats.check(text)).to eq("Hello")
+      result = grammerStats.check("hello")
+      expect(result).to eq(false)
+    end
+    
+    it "Should return as FALSE when string does start with a capital letter or a punctuation mark." do
+      grammerStats = GrammarStats.new
+      result = grammerStats.check("Hello")
+      expect(result).to eq(false)
+    end
+
+    it "Should return as FALSE when the string starts without a capital letter and  ends with the correct punctuation mark." do
+      grammerStats = GrammarStats.new
+      result = grammerStats.check("hello.")
+      expect(result).to eq(false)
+    end
+
+    it "Should return as TRUE when the string starts with a capital letter and ends with a punctuation mark." do
+      grammerStats = GrammarStats.new
+      result = grammerStats.check("Hello.")
+      expect(result).to eq(true)
     end
   end
 end
