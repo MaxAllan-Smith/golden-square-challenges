4fe9f1b256d2c48656af188854050c13f8aafadf
Max Allan-Smith <Maxibon_1195@hotmail.com>
2023-01-21 17:42:40 +0000

    Initiliazed Todo class

---
+++ Design-Class-Program-Design-Recipe/Exercise/lib/todo.rb
@@ -0,0 +1,23 @@
+
+
+class Todo
+  
+  def initialize(title, description, completed)
+    @title = title
+    @description = description
+    @completed = completed
+  end
+
+  def add_task
+    
+  end
+
+  def view_task
+    
+  end
+
+  def delete_task
+    
+  end
+
+end
